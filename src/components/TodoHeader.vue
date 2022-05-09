414e3ad24a11c63027bce3d948fc5c65cc6cbaa3
salangdung-i <dladmswl92@naver.com>
2022-05-09 21:20:00 +0900

    TodoHeader 컴포넌트 CSS 추가

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>Header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+/* 
+  scoped는 뷰에서 지원하는 속성이며, 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미
+ */
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
