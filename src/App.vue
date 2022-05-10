be2fe515faf1b5645653f69cacaa92bede041160
salangdung-i <dladmswl92@naver.com>
2022-05-10 21:08:17 +0900

    props와 이벤트 전달을 통해 할일 입력 기능 개선하기

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-bind:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -14,6 +14,36 @@ import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
 
 export default {
+  // data 영역
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  // 사용하는 함수들을 모아놓은 method 영역
+  methods: {
+    addTodo(todoItem) {
+      // 로컬 스토리지에서 데이터를 추가하는 로직
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  // components 영역
   components: {
     TodoHeader: TodoHeader,
     TodoInput: TodoInput,
